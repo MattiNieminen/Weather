e4c4dbfd2ddb60837d2994eeaed386a7b99588be
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-06-14 10:39:56 +0300

    Implemented database caching for history api.

--- app/src/weather/weather_history_api.clj
+++ app/src/weather/weather_history_api.clj
@@ -3,6 +3,7 @@
             [clojure.xml :as xml]
             [clojure.zip :as zip]
             [clojure.string :as str]
+            [monger.collection :as mc]
             [weather.utils :as utils]))
 
 (def WeatherHistory {:fog s/Num
@@ -54,9 +55,25 @@
   (xml/parse (str "http://api.wunderground.com/api/" apikey "/history_"
                   (str/replace date #"-" "") "/q/" city ".xml")))
 
+(defn get-weather-from-database
+  [database city date]
+  (:wg-data (mc/find-one-as-map database :weather-history {:city city
+                                                           :date date})))
+
+(defn insert-weather-to-database
+  [database city date wg-data]
+  (mc/insert database :weather-history {:_id (utils/object-id)
+                                        :city city
+                                        :date date
+                                        :wg-data wg-data}))
+
 (defn get-weather
   [wg-apikey database city date]
-  (let [wg-data (get-weather-from-wg wg-apikey date city)]
-    (if (wg-xml-has-data? wg-data)
-      (->WeatherHistory wg-data)
-      {})))
+  (if-let [wg-data (get-weather-from-database database city date)]
+    (->WeatherHistory wg-data)
+    (let [wg-data (get-weather-from-wg wg-apikey date city)]
+      (if (wg-xml-has-data? wg-data)
+        (do
+          (insert-weather-to-database database city date wg-data)
+          (->WeatherHistory wg-data))
+        {}))))
--- app/test/weather/weather_history_api_test.clj
+++ app/test/weather/weather_history_api_test.clj
@@ -2,10 +2,18 @@
   (:require [clojure.test :refer :all]
             [weather.weather-history-api :refer :all]
             [clojure.java.io :as io]
-            [clojure.xml :as xml]))
+            [clojure.xml :as xml]
+            [weather.test-utils :as test-utils]
+            [clj-time.core :as t]
+            [monger.collection :as mc]))
+
+(use-fixtures :each test-utils/database-fixture)
 
 (def history-xml (xml/parse "resources/test/history.xml"))
 (def empty-history-xml (xml/parse "resources/test/empty_history.xml"))
+
+(def database (:database (:mongodb test-utils/test-system)))
+(def wg-apikey (get-in test-utils/test-system [:config :loaded :wg-apikey]))
 
 (deftest wg-xml-has-data?-test
   (testing "Xmls without data are identified without exceptions."
@@ -25,3 +33,9 @@
    (is (= (->WeatherHistory history-xml)
           {:fog 1 :rain 1 :snow 1 :hail 0
            :meantempm 2 :maxtempm 3 :mintempm 1}))))
+
+(deftest get-weather-test
+  (testing "Weather data is fetched from database if it exists."
+   (is (mc/empty? database :weather-history))
+   (get-weather wg-apikey database "Tampere" (t/local-date 2014 1 1))
+   (is (= 1 (mc/count database :weather-history)))))
